41810621dd884444949c269a941728e878ba171c
jojonki <junki.ohmura@gmail.com>
2018-03-13 16:41:27 -0400

    add insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,13 @@
+N = int(input())
+A = [int(i) for i in input().split(' ')]
+print(A)
+
+for i in range(1, N):
+    v = A[i]
+    j = i - 1
+    while j >= 0 and A[j] > v:
+        A[j+1] = A[j]
+        j -= 1
+    A[j+1] = v
+
+print(A)
