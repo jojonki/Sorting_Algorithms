3e9c96e9a297a376cf6cf1bfc83a503eeb5bebd0
jojonki <junki.ohmura@gmail.com>
2018-03-16 10:20:49 -0400

    fix counter bug

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,15 +4,16 @@ N = int(input())
 A = [int(i) for i in input().split(' ')]
 
 swap_count = 0
+i = 0
 while True:
     crnt_swap_count = swap_count
-    for i in reversed(range(1, N)):
-        if A[i-1] > A[i]:
-            tmp = A[i]
-            A[i] = A[i-1]
-            A[i-1] = tmp
-            swap = True
+    for j in reversed(range(i + 1, N)):
+        if A[j-1] > A[j]:
+            tmp = A[j]
+            A[j] = A[j-1]
+            A[j-1] = tmp
             swap_count += 1
+    i += 1
     if crnt_swap_count == swap_count: # no swap
         break
 
